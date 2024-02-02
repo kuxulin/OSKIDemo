0c3050fd9f4c8a2fb7d548f94488acef946667a6
Maks <Maks>
2024-02-02 16:59:38 +0200

    Added swagger with some services

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using OSKIDemo.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options => 
+         options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnectionString")));
+
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<DataContext>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.Run();
